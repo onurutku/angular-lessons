7c16ba91d503bfa4a37939c532d90d4275f1b7f1
onurutku <topaloglu08@gmail.com>
2021-12-09 15:27:14 +0300

    initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-firs-app';
+  title = 'ilk uygulama koççum';
+  name="kabak";
 }
